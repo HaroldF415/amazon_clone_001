061b019ef608e3d297232ac273f104347ac3c22b
joshuanelsondev <joshuanelsondev@gmail.com>
2023-02-25 12:20:08 -0500

    Style the header
    add icons and text to the header and adjust colors and sizing.
    Will need to add Amazon ember font.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,27 @@
-
+import SearchBar from "./SearchBar";
+import "../styles/NavBar.css";
+import amazonLogo from "../Icons/AmazonLogo.png"
+import locationIcon from "../Icons/locationIcon.png"
+import shoppingCart from "../Icons/shoppingCart.png"
 
 const NavBar = () => {
   return (
     <header>
-      <h1>Lmooo</h1>
+      <img src={amazonLogo} alt="Amazon Logo" className="amazonLogo" />
+      <div>
+        <img src={locationIcon} alt="Location Icon" />
+        <p>Hello</p>
+        <p>Select your address</p>
+      </div>
+      <SearchBar />
+      <div>
+        <span>Hello, sign in</span><span>Account & Lists</span>
+        
+      </div>
+      <div>
+        <span></span>Returns<span>& Orders</span>
+      </div>
+      <img src={shoppingCart} alt="Shopping Cart Icon" className="shoppingCart" />
     </header>
   );
 };
