0b7ed9706a48cfb2f3a2a1bdec092cac945894ac
joshuanelsondev <joshuanelsondev@gmail.com>
2023-02-28 18:43:20 -0500

    Structure the jsx of the left side of the navbar within NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,14 +6,27 @@ import shoppingCart from "../Icons/shoppingCart.png"
 
 const NavBar = () => {
   return (
-    <nav>
-      <img src={amazonLogo} alt="Amazon Logo" className="amazonLogo" />
-      <div className="locationDiv">
-        <img src={locationIcon} alt="Location Icon" />
-        <p>Hello</p>
-        <p>Select your address</p>
+    <header id="navbar-main">
+      <div id="navbar">
+        <div id="nav-belt">
+          <div class="nav-left">
+            <div id="nav-logo">
+              <img src={amazonLogo} alt="Amazon Logo" className="amazonLogo" />
+            </div>
+            <div id="nav-global-location">
+              <img src={locationIcon} alt="Location Icon" />
+              <p>Hello</p>
+              <p>Select your address</p>
+            </div>
+          </div>
+          <div class="nav-fill">
+            <div id="nav-search">
+              <SearchBar />
+            </div>
+          </div>
+          <div class="nav-right"></div>
+        </div>
       </div>
-      <SearchBar />
       <div>
         <span>Hello, sign in</span><span>Account & Lists</span>
         
@@ -22,7 +35,7 @@ const NavBar = () => {
         <span></span>Returns<span>& Orders</span>
       </div>
       <img src={shoppingCart} alt="Shopping Cart Icon" className="shoppingCart" />
-    </nav>
+    </header>
   );
 };
 export default NavBar;
